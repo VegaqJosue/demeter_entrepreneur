833ca51f2f0bc493f257b7ee4927b7db11166f39
Josue Francisco Orellana Vega <orellana.vega@gmail.com>
2026-02-11 12:04:22 -0600

    base commit

---
+++ core/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+
+@login_required
+def home(request):
+    return render(request, "home.html")
+
+
