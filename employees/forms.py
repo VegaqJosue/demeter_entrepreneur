290a155f21ab9140f707e8075dfcb6f98f1934b9
Josue Francisco Orellana Vega <orellana.vega@gmail.com>
2026-03-06 13:06:00 -0600

    Las modification to the employee App, create and edit employees are working as expected

--- employees/forms.py
+++ employees/forms.py
@@ -5,4 +5,23 @@ from .models import Employee
 class EmployeeForm(forms.ModelForm):
     class Meta:
         model = Employee
-        fields = "__all__"
+        fields = [
+            'employee_code',
+            'first_name',
+            'second_name',
+            'third_name',
+            'first_surname',
+            'second_surname',
+            'document_name',
+            'document_id',
+            'photo',
+            'credit_limit',
+        ]
+        
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args,**kwargs)
+        for name, field in self.fields.items():
+            if self.errors.get(name):
+                field.widget.attrs.update({'class': 'form-control is-invalid'})
+            else:
+                field.widget.attrs.update({'class': 'form-control'})
